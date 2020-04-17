a70661d3bd802c861c2b2056a14e2c873f4fa558
Andrew Porter <andy@plingit.co.uk>
2020-04-17 17:37:40 +0100

    Fixed helper names

--- lib/generators/bs_scaffold/templates/helper.rb
+++ lib/generators/bs_scaffold/templates/helper.rb
@@ -1,13 +1,13 @@
 module <%= class_name.pluralize %>Helper
-  def sort_link
+  def <%= class_name.pluralize %>_sort_link
     @link ||= lambda { |params| <%= plural_table_name %>_path( params ) }
   end
 
-  def next_page ( <%= plural_table_name %> )
-    return <%= plural_table_name %>_path( safe_params(params).merge page: <%= plural_table_name %>.next_page ) if <%= plural_table_name %>.next_page
+  def <%= class_name.pluralize %>_next_page ( <%= plural_table_name %> )
+    return <%= plural_table_name %>_path( <%= class_name.pluralize %>_safe_params(params).merge page: <%= plural_table_name %>.next_page ) if <%= plural_table_name %>.next_page
   end
 
-  def safe_params(p)
+  def <%= class_name.pluralize %>_safe_params(p)
     p.permit(:sort, :direction, :search)
   end
 
