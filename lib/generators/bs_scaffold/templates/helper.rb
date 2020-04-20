9643de675a054c56ddace35fc774ac8dca7b2e33
Andrew Porter <andy@plingit.co.uk>
2020-04-20 12:17:29 +0100

    Wrong method names in helper

--- lib/generators/bs_scaffold/templates/helper.rb
+++ lib/generators/bs_scaffold/templates/helper.rb
@@ -1,13 +1,13 @@
 module <%= class_name.pluralize %>Helper
-  def <%= class_name.pluralize %>_sort_link
+  def <%= plural_table_name %>_sort_link
     @link ||= lambda { |params| <%= plural_table_name %>_path( params ) }
   end
 
-  def <%= class_name.pluralize %>_next_page ( <%= plural_table_name %> )
-    return <%= plural_table_name %>_path( <%= class_name.pluralize %>_safe_params(params).merge page: <%= plural_table_name %>.next_page ) if <%= plural_table_name %>.next_page
+  def <%= plural_table_name %>_next_page ( <%= plural_table_name %> )
+    return <%= plural_table_name %>_path( <%= plural_table_name %>_safe_params(params).merge page: <%= plural_table_name %>.next_page ) if <%= plural_table_name %>.next_page
   end
 
-  def <%= class_name.pluralize %>_safe_params(p)
+  def <%= plural_table_name %>_safe_params(p)
     p.permit(:sort, :direction, :search)
   end
 
